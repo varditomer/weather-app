2deb6230a897f6f5a2ca7cf48f994dc089eff61d
varditomer <tomer.vardi@gmail.com>
2023-11-02 18:27:01 +0200

    add locations, weather, http services

---
+++ src/services/http.service.ts
@@ -0,0 +1,19 @@
+import Axios from "axios"
+
+export const httpService = {
+  get(url: string) {
+    return ajax(url, "GET")
+  },
+}
+
+async function ajax(url: string, method = "GET") {
+  try {
+    const res = await Axios({
+      url,
+      method,
+    })
+    return res.data
+  } catch (err: any) {
+    console.error(err)
+  }
+}
--- src/services/locations.service.ts
+++ src/services/locations.service.ts
@@ -1,6 +1,10 @@
+import { httpService } from "./http.service"
+
 const getLocations = async (locationName: string) => {
   const apikey = process.env.REACT_APP_WEATHER_API_KEY
-  const getLocationsUrl = `https://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${apikey}&q=lon`
+  const getLocationsUrl = `https://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${apikey}&q=${locationName}`
+  const locations = await httpService.get(getLocationsUrl)
+  console.log(`locations:`, locations)
 }
 
 export const locationService = {
