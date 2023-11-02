c3876665d662dd131864578d979e6790f17e168c
varditomer <tomer.vardi@gmail.com>
2023-11-02 23:50:31 +0200

    weather item inner layout strcture and style

--- src/components/LocationWeatherItem.tsx
+++ src/components/LocationWeatherItem.tsx
@@ -1,19 +1,36 @@
 import { LocationWeather } from "../interfaces/LocationWeather.interface"
-import rectangleElement from '../assets/elements/rectangle.svg'
+import weatherImg from '../assets/elements/weather/Weather - Search/Big/moon_cloud_mid _rain.png'
 
 type LocationWeatherItemProps = {
-    location: LocationWeather
+  location: LocationWeather
 }
 
 export default function LocationWeatherItem({ location }: LocationWeatherItemProps) {
   return (
-      <li
-        className="location-weather-item"
-        key={location.id}
-      >
-        
-        {/* <img src={rectangleElement} alt="" className="rectangle" /> */}
-        <span>{location.name}</span>
-      </li>
+    <li
+      className="location-weather-item"
+      key={location.id}
+    >
+      <div className="info-section">
+        <div className="curr-temp">{location.temp}</div>
+        <div className="details">
+          <div className="temp-range">
+            <span className="high"></span>
+            <span className="low"></span>
+          </div>
+          <div className="location-details">
+            <span className="city">{location.name}</span>
+            <span>, </span>
+            <span className="country">{location.country}</span>
+          </div>
+        </div>
+
+      </div>
+
+      <div className="weather-type-section">
+        <img src={weatherImg} alt="" className="weather-element"/>
+        <div className="weather-desc">{location.weather}</div>
+      </div>
+    </li>
   )
 }
